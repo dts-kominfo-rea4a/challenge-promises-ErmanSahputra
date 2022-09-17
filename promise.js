033a9a3ebe99773fcd2479db53690d37e3473ad3
ASUS K 46 CM <ermansahputra.com>
2022-09-17 15:58:04 +0700

    promise

--- promise.js
+++ promise.js
@@ -1,8 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
-
+const promiseOutput = async (parameter) => {
+  const merged = [...await promiseTheaterIXX(), ...await promiseTheaterVGC()];
+  let counter = 0;
+  merged.forEach(jumlah => {
+    if (jumlah.hasil === parameter) {
+      counter++;
+    }
+  });
+  return counter;
+};
+  
 module.exports = {
   promiseOutput,
 };
